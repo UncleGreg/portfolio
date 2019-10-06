3c4943920409013b8ccf35cce156370f8705cd37
UncleGreg <grzesiekwitczak@gmail.com>
2019-10-06 11:26:43 +0200

    added MainTemplate, GlobalStyles, Theme

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,5 @@
+export const theme = {
+  primary: 'red',
+};
+
+export default theme;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import MainTemplate from 'templates/MainTemplate/MainTemplate';
+import styled from 'styled-components';
+
+const StyledHeading = styled.h1`
+  color: red;
+`;
 
 const IndexPage = () => (
-  <div>
-    <h1>Hello World</h1>
-  </div>
+  <MainTemplate>
+    <StyledHeading>Front-end Developer</StyledHeading>
+    <StyledHeading>Front-end Developer</StyledHeading>
+  </MainTemplate>
 );
 
 export default IndexPage;
